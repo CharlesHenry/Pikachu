7e38d37d74adad8c095833f450acdfb24a8e748f
Charles Henry <charles.henry@live.com>
2014-01-30 18:55:16 +0000

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int myNum1 = 0;
+    int myNum2 = 0;
+    int finalNum = 0;
+
+    cout << "Enter the first number bruv!\n";
+    cin >> myNum1;
+
+    cout << "Enter the second number bruv!\n";
+    cin >> myNum2;
+
+    finalNum = myNum1 + myNum2;
+    cout << "You must be crazy mate, " << myNum1 << " + " << myNum2 << " = " << finalNum << "!\n";
+
+    return 0;
+}
